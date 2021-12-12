a2edb7d5c7e9188a7601493d78d6ca755fa5ef62
CaniTanushaj <hasan.tanushaj@fsre.sum.com>
2021-12-12 21:56:28 +0100

    prvi commit

---
+++ resources/views/Onama.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Auto salon App</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/main.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+  </head>
+  <body>
+    <nav>
+      <input type="checkbox" id="check">
+      <label for="check" class="checkbtn">
+        <i class="fas fa-bars"></i>
+      </label>
+      <label class="logo">Auto Salon App</label>
+      <ul>
+        <li><a href="/">Početna</a></li></a>
+        <li><a class="active" href="/Onama">O nama</a></li>
+        @if (Route::has('login'))
+        @auth
+        <li> <a href="{{ url('/home') }}" class="">Odjava</a></li>
+        @else
+        <li> <a href="{{ route('login') }}" class="">Prijava</li>
+        @if (Route::has('register'))
+        <li><a href="{{ route('register') }}" class="">Registracija</a></li>   
+      
+      @endif
+                    @endauth
+    
+    @endif
+
+      </ul>
+    </nav>
+    <div class="caca">
+    <div class="box"><img src="/img/ht.jpeg" class="slike">
+      <li>Ime i prezime: Hasan Tanushaj</li>
+      <li>Grad: Livno</li>
+      <li>Kontakt: 063165448</li>
+  </div>
+    <div class="box2"><h1 style="margin: 10px; color: white;">Nešto o nama i o projektu</h1>
+    <p style="padding: 10px; font-size: 30px;">
+        Hasan Tanushaj i Mateo Vukoja su studenti treće godine Fakulteta strojarstva, računarstva i elektrotehnike sveučilišta u Mostaru. Kao veliki obožavatelji automobila odlučili smo kroz projekt omogućiti olakšice kod procesa kupnje, servisiranja i potrage odgovarajućeg automobila. Kroz našu aplikaciju možete personalizirati ponudu automobila i raznih usluga koje se nude na tržištu. Na prvom mjestu nam je ostvarivanje korisnikovih zahtjeva i uspješna suradnja. 
+    </p>
+    </div>
+    <div class="box"><img src="/img/MV.jpeg" class="slike">
+    <li>Ime i prezime: Mateo Vukoja</li>
+      <li>Grad: Široki Brijeg</li>
+      <li>Kontakt: 063176304</li></div>
+
+    </div>
+
+  </body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<!-- Created By CodingNepal - www.codingnepalweb.com -->
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Auto salon App</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/main.css">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+  </head>
+  <body>
+  
+    <nav>
+      <input type="checkbox" id="check">
+      <label for="check" class="checkbtn">
+        <i class="fas fa-bars"></i>
+      </label>
+      <label class="logo">Auto Salon App</label>
+      <ul>
+        <li><a class="active" href="/">Početna</a></li>
+        <li><a href="/Onama">O nama</li></a>
+        @if (Route::has('login'))
+        @auth
+        <li> <a href="{{ url('/home') }}" class="">Odjava</a></li>
+        @else
+        <li> <a href="{{ route('login') }}" class="">Prijava</a></li>
+        @if (Route::has('register'))
+        <li><a href="{{ route('register') }}" class="">Registracija</a></li>   
+      
+      @endif
+                    @endauth
+    
+    @endif
+
+      </ul>
+    </nav>
+    <section></section>
+  </body>
+</html>
